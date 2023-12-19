b736f0dbb4a43ac52291b6b2d90cadc184f814e0
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-19 15:11:05 +0000

    Added shadcn-ui button
    Also moved the page.tsx to root

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <p>
+        Hello Admin Dashboard
+      </p>
+      <Button size="sm" variant="destructive">Click Me</Button>
+    </>
+  )
+}
